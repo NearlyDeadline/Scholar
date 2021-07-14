a0455bd7a6747cbfb491e0891e04ffd9d0cbdfe4
NearlyDeadline <1289790957@qq.com>
2021-07-14 10:45:14 +0800

    修正文件头

--- web_of_science_crawler/web_of_science_crawler/spiders/AdvancedQuery.py
+++ web_of_science_crawler/web_of_science_crawler/spiders/AdvancedQuery.py
@@ -1,7 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/7/14 10:43
+# @Author  : Mike
+# @File    : AdvancedQuery.py
 import scrapy
 
 
-class AdvancedquerySpider(scrapy.Spider):
+class AdvancedQuerySpider(scrapy.Spider):
     name = 'AdvancedQuery'
     allowed_domains = ['https://www.webofknowledge.com']
     start_urls = ['http://https://www.webofknowledge.com/']
