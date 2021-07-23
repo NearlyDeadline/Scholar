147c30081d7494fb6ac5250ccfe680492e030954
NearlyDeadline <1289790957@qq.com>
2021-07-23 19:14:16 +0800

    XLSParser

--- rank/rank.py
+++ rank/rank.py
@@ -44,11 +44,46 @@ class DBLParser:
                 query_file.write('\n' + self.title_list[i])
 
 
+class Contribution:
+    paper_title = ''
+    paper_path = ''
+    is_first_author = False
+    is_corresponding_author = False
+
+
 # 遍历output_dir下每个xls文件，识别第一作者/通讯作者
 class XLSParser:
+    title_contribution_dict = {}
+
     # contribution_path: 每一行表明该作者与一篇论文的关系，应包含：论文元信息（题目等），以及作者的贡献（一作/通讯/非一作非通讯/未知）
     def __init__(self, papers_dir: str, author_name: str, contribution_path: str):
-        pass
+        for xls_path in os.listdir(papers_dir):
+            xls_data = pd.read_excel(papers_dir + '/' + xls_path)
+            xls_title = xls_data['Article Title'][0]
+            xls_first_author = xls_data['Author Full Names'][0].strip(';')
+            xls_first_author = ''.join(list(filter(lambda c: str.isalpha(c), xls_first_author[0].lower())))
+
+            def getCorrespondingAuthor(reprint_addresses: str) -> str:
+                authors = reprint_addresses.split('corresponding author')
+                if len(authors) == 1:
+                    authors = reprint_addresses.split('Corresponding author')
+                if len(authors) > 1:
+                    return ''.join(list(filter(lambda c: str.isalpha(c), authors[0].lower())))
+                else:
+                    return ''
+
+            xls_corresponding_author = getCorrespondingAuthor(xls_data['Reprint Addresses'][0])
+            author_name = ''.join(list(filter(lambda c: str.isalpha(c), author_name.lower())))
+
+            pc = Contribution()
+            pc.paper_title = xls_title
+            pc.paper_path = xls_path
+            if author_name.startswith(xls_first_author):
+                pc.is_first_author = True
+            if author_name.startswith(xls_corresponding_author):
+                pc.is_corresponding_author = True
+
+            self.title_contribution_dict[xls_title] = pc
 
 
 # 识别为相应作者的，查表获取论文排名；识别不是相应作者的，丢弃；无法确定的，单独输出
@@ -59,6 +94,17 @@ class Ranker:
 
 
 if __name__ == '__main__':
+    author_name = ['Khosla, Megha', 'Setty, Vinay', 'Anand, Avishek']
+    for an in author_name:
+        x = XLSParser('../test/xls', an, '../test/xls' + an + '.txt')
+        for k, v in x.title_contribution_dict.items():
+            print(f'key: {k}')
+            print(f'paper_path: {v.paper_path}')
+            print(f'is_first_author: {v.is_first_author}')
+            print(f'is_corresponding_author: {v.is_corresponding_author}')
+
+
+def main():
     dblp_raw_file = '../test/北京交通大学_article.csv'
     wos_input_file = '../test/北京交通大学_article.txt'
     d = DBLParser(dblp_raw_file, wos_input_file)
@@ -68,7 +114,7 @@ if __name__ == '__main__':
 
     a = AdvancedQuerySpiderRunner(wos_input_file, xls_dir, output_format='saveToExcel', error_log_path=error_log_path)
 
-    author_contribution_path = '../test/contribution.txt'
+    author_contribution_path = '../test/title_contribution_dict.txt'
     x = XLSParser(xls_dir, d.author_name, author_contribution_path)
 
     rank_path = '../test/rank.txt'
