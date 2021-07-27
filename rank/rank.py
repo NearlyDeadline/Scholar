f0c795e7384e4af0f63cf2016da670b7d2b4bbb4
NearlyDeadline <1289790957@qq.com>
2021-07-27 14:05:58 +0800

    修正函数名

--- rank/rank.py
+++ rank/rank.py
@@ -2,13 +2,11 @@
 # @Time    : 2021/7/14 15:09
 # @Author  : Mike
 # @File    : get_rank_json
-from web_of_science_crawler.main import AdvancedQuerySpiderRunner
 from dblparser import DBLParser
 from xlsparser import XLSParser
 from achievement import Achievement
 import json
 import os
-import sys
 
 if __name__ == '__main__':
     dblp_raw_file = '../test/北京交通大学/北京交通大学_BaopengZhang/BaopengZhang_article.csv'
@@ -22,30 +20,8 @@ if __name__ == '__main__':
 
     x = XLSParser(xls_dir, d.author_name)
     a = Achievement()
-    a.dblp(d)
-    a.wos(x)
+    a.load_dblp(d)
+    a.load_wos(x)
     rank_json = a.get_rank_json()
     with open(dblp_raw_dir + 'achievement.json', 'w', encoding='utf-8') as f:
         f.write(json.dumps(rank_json, indent=4, ensure_ascii=False))
-    achi_csv_path = dblp_raw_dir
-    a.save_as_csv(achi_csv_path)
-
-
-def main():
-    dblp_raw_file = '../test/北京交通大学_article.csv'
-    d = DBLParser(dblp_raw_file)
-
-    wos_input_file = '../test/北京交通大学_article.txt'
-    d.save_for_spider(wos_input_file)
-    xls_dir = '../test/xls'
-    error_log_path = wos_input_file + '.log'
-    AdvancedQuerySpiderRunner(wos_input_file, xls_dir, output_format='saveToExcel', error_log_path=error_log_path)
-    x = XLSParser(xls_dir, d.author_name)
-
-    a = Achievement()
-    a.dblp(d)
-    a.wos(x)
-    rank_json = a.get_rank_json()
-
-    achi_csv_path = '../test/contrib/' + d.author_name + '.csv'
-    a.save_as_csv(achi_csv_path)
